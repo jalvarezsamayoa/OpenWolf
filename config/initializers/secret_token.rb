ffe81367e3b55c49208952fec44184dde737931f
Javier Alvarez <jalvarezsamayoa@gmail.com>
2010-12-29 10:50:39 -0600

    Cambio a postgreql; remover prototype

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OpenwolfV3::Application.config.secret_token = '4751706daa6165b5bef38d7fde67224909e674f178950aabb179366e3e40f6348630add5096fba995491b8bbf97101f7dd11405198ae4a52c9ec504da5d39bb5'
+OpenwolfV3::Application.config.secret_token = '7b74f998516016b8e58edbed54bbe2dccb5eca1333b3fa0255a8db1ecd69963a6703907d2f89c5ad31cc3bf3e9a7a9803e1000592257d65ca37f7a3bf6288c9b'

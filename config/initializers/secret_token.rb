3d64199205efac3b3f047f842d157e6f98f07dc8
Javier Alvarez <jalvarezsamayoa@gmail.com>
2010-12-29 10:44:36 -0600

    Carga inicial rails 3 branch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenwolfV3::Application.config.secret_token = '4751706daa6165b5bef38d7fde67224909e674f178950aabb179366e3e40f6348630add5096fba995491b8bbf97101f7dd11405198ae4a52c9ec504da5d39bb5'

1213c0f72ce37360d1937ceff3449f6b68dd90ae
Javier Alvarez <jalvarezsamayoa@gmail.com>
2011-04-19 22:41:35 -0600

    fix

--- db/migrate/20110420041440_add_positiva_to_tiporesoluciones.rb
+++ db/migrate/20110420041440_add_positiva_to_tiporesoluciones.rb
@@ -1,6 +1,6 @@
 class AddPositivaToTiporesoluciones < ActiveRecord::Migration
   def self.up
-    add_column :tiposresoluciones, :positiva, :boolean, :default => 0
+    add_column :tiposresoluciones, :positiva, :boolean, :default => false
     add_column :tiposresoluciones, :aliaspdh, :string
 
     add_index :tiposresoluciones, :positiva

b9b7d05ccd60ae18f4041ba3faa6bc1a015fbea3
Javier Alvarez <jalvarezsamayoa@gmail.com>
2011-06-02 07:31:03 -0600

    Reconfigurado server de correo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,14 +51,14 @@ OpenwolfV3::Application.configure do
   #configuracion de dominio predeterminado para DEVISE
   config.action_mailer.default_url_options = { :host => 'transparencia.gob.gt' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'openwolf.org',
-    :user_name            => 'no-reply@openwolf.org',
-    :password             => 'Bless777',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'openwolf.org',
+  #   :user_name            => 'no-reply@openwolf.org',
+  #   :password             => 'Bless777',
+  #   :authentication       => 'plain',
+  #   :enable_starttls_auto => true  }
  
 end

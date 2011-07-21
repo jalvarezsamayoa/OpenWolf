f4b8bc4623bc55293df1be4cad37afa793cf6349
Javier Alvarez <jalvarezsamayoa@gmail.com>
2011-07-20 23:08:47 -0600

    agregado spec para clasificacion;

--- spec/models/clasificacion_spec.rb
+++ spec/models/clasificacion_spec.rb
@@ -3,17 +3,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Clasificacion do
   
   before(:each) do
-    @clasificacion = Factory(:clasificacion)
+    @clasificacion = Factory.build(:clasificacion)
   end
   
   it "debe ser valido" do
     @clasificacion.should be_valid
   end
   
-  describe '#metodo' do    
-    it 'debe hacer algo' do
-      pending
-    end    
-  end
-  
+   
 end

f056bac6e4d506575c5016b73082f5433dfd1b00
Javier Alvarez <jalvarezsamayoa@gmail.com>
2011-07-21 09:23:21 -0600

    actualizado spec para modelo fuent

--- spec/models/fuente_spec.rb
+++ spec/models/fuente_spec.rb
@@ -3,17 +3,12 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Fuente do
   
   before(:each) do
-    @fuente = Factory(:fuente)
+    @fuente = Factory.build(:fuente)
   end
   
   it "debe ser valido" do
     @fuente.should be_valid
   end
   
-  describe '#metodo' do    
-    it 'debe hacer algo' do
-      pending
-    end    
-  end
   
 end

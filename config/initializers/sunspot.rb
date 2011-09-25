6e79b0abf7f9288ecc4949081173610bde44a561
Javier Alvarez <jalvarezsamayoa@gmail.com>
2011-09-24 22:15:53 -0600

    fix

--- config/initializers/sunspot.rb
+++ config/initializers/sunspot.rb
@@ -1,14 +1,10 @@
 case Rails.env
 when 'development'
   Sunspot.config.solr.url = 'http://locahost:8982/solr'
-  Sunspot.config.solr.port = 8982
 when 'test'
   Sunspot.config.solr.url = 'http://locahost:8983/solr'
-  Sunspot.config.solr.port = 8983
 when 'production'
   Sunspot.config.solr.url = 'http://locahost:8983/solr'
-  Sunspot.config.solr.port = 8983
 when 'staging'
   Sunspot.config.solr.url = 'http://locahost:8984/solr'
-  Sunspot.config.solr.port = 8984
 end

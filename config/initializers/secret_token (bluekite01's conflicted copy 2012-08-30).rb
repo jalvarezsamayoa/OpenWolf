53484d87e1684f1258db3fae93618dfe55af33cc
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-21 12:37:45 -0600

    remove all dropbox conflicts

--- config/initializers/secret_token (bluekite01's conflicted copy 2012-08-30).rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-OpenwolfV3::Application.config.secret_token = '9bec2bc4adf2f322b54463704f0c277c1b8e5bd94ab3cb75709ce3d259a6f8dd853d1d0838377928c0f6f6fcba7d8baf7e0cf2807a3e3ef7337e26a0b0e629ef'
